983b723aae29237e76af1193694a169208b4d0ed
Slava Koshman <koshman2015@list.ru>
2024-05-24 11:03:01 +0000

    feat: changes from server

--- src/worker.py
+++ src/worker.py
@@ -1,9 +1,10 @@
 import os
-from time import perf_counter, sleep
+from time import perf_counter
+
 from celery import Celery
 from celery.contrib.abortable import AbortableTask
 import pandas as pd
-import numpy as np
+from smbus2 import SMBus
 
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
@@ -15,14 +16,17 @@ def session(self: AbortableTask, filename: str):
     count = 0
     data = []
     t0 = perf_counter()
-    while True:
-        # Logic to read data
-        e1, e2, e3 = np.random.rand(), np.random.rand(), np.random.rand()
-        data.append([perf_counter() - t0, e1, e2, e3])
-        if count == 1000:
-            if self.is_aborted():
-                pd.DataFrame(data.copy(), columns=["time", "y1", "y2", "y3"]).to_csv(f"static/{filename}", index=False)
-                return "Finished"
-            count = 0
-        count += 1
-        sleep(0.0025)
+    with SMBus(3) as bus:
+        bus.pec = 1
+        bus.read_i2c_block_data(0x42, 0, 24)
+        while True:
+            # Logic to read data
+            msg = bytes(filter(lambda x:x != 255, bus.read_i2c_block_data(0x42, 0, 24))).decode("utf-8").split(",")
+            data.append([perf_counter() - t0, *msg])
+            if count == 1000:
+                if self.is_aborted():
+                    pd.DataFrame(data.copy(), columns=["time", "y1", "y2", "y3"]).to_csv(f"static/{filename}", index=False)
+                    return "Finished"
+                count = 0
+            count += 1
+
